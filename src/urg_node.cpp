fc3f95e7e07a3ca7c67e6ee437cf3c3829a49302
Aarush Gupta <aarush@hopetechnik.com>
2018-06-22 05:09:03 +0530

    Added support for parameters in urg_node.cpp

--- src/urg_node.cpp
+++ src/urg_node.cpp
@@ -30,7 +30,9 @@
 /*
  * Author: Chad Rockey, Michael Carroll, Mike O'Driscoll
  */
-
+ #include <chrono>
+ #include <memory>
+ #include <sstream>
 #include "urg_node/urg_node_driver.h"
 
 // boost headers
@@ -39,6 +41,39 @@
 // rcutils headers
 #include <rcutils/cmdline_parser.h>
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+void on_parameter_event(
+  const rcl_interfaces::msg::ParameterEvent::SharedPtr event, rclcpp::Logger logger)
+{
+  // TODO(wjwwood): The message should have an operator<<, which would replace all of this.
+  std::stringstream ss;
+  if(!event->new_parameters.empty())
+  {
+    ss << "\nParameter event:\n new parameters:";
+    for (auto & new_parameter : event->new_parameters) {
+      ss << "\n  " << new_parameter.name;
+    }
+  }
+  if(!event->changed_parameters.empty())
+  {
+    ss << "\n changed parameters:";
+    for (auto & changed_parameter : event->changed_parameters) {
+      ss << "\n  " << changed_parameter.name;
+    }
+  }
+  if(!event->deleted_parameters.empty())
+  {
+    ss << "\n deleted parameters:";
+    for (auto & deleted_parameter : event->deleted_parameters) {
+      ss << "\n  " << deleted_parameter.name;
+    }
+  }
+  ss << "\n";
+  RCLCPP_INFO(logger, ss.str().c_str())
+}
+
 
 int main(int argc, char **argv)
 {
@@ -46,7 +81,31 @@ int main(int argc, char **argv)
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("urg_node");
 
+
+  //Create a Sync param service client, each node already has a param service by default
+  auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(node);
+  std::chrono::seconds sec(1);
+  while (!parameters_client->wait_for_service(sec)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.")
+      return 0;
+    }
+    RCLCPP_INFO(node->get_logger(), "service not available, waiting again...")
+  }
+
+
+  // Setup callback for changes to parameters.
+  auto sub_params = parameters_client->on_parameter_event(
+    [node](const rcl_interfaces::msg::ParameterEvent::SharedPtr event) -> void
+    {
+      on_parameter_event(event, node->get_logger());
+    });
+
   // Support the optional serial port command line argument
+  /*
+  *Switched to using parameters loaded via a yaml file, rather than command line arguments
+  *
+  *
   std::string serialPort = "/dev/ttyACM0";
   std::string option = "--serial-port";
   if (rcutils_cli_option_exist(argv, argv + argc, option.c_str())) {
@@ -82,14 +141,17 @@ int main(int argc, char **argv)
   if (rcutils_cli_option_exist(argv, argv + argc, option.c_str())) {
     laserFrameId = rcutils_cli_get_option(argv, argv + argc, option.c_str());
   }
+  */
 
   urg_node::UrgNode urgNode;
 
+
   // Update settings
-  urgNode.setSerialPort(serialPort);
-  urgNode.setUserLatency(userLatency);
-  urgNode.setIPAdddress(ipAddress);
-  urgNode.setIPPort(ipPort);
+  urgNode.setSerialPort(parameters_client->get_parameter<std::string>("serial_port","/dev/ttyACM0"));
+  urgNode.setUserLatency(parameters_client->get_parameter<double>("user_latency",0.0));
+  urgNode.setIPAdddress(parameters_client->get_parameter<std::string>("ip_address",""));
+  urgNode.setIPPort(parameters_client->get_parameter<int>("ip_port",0));
+  urgNode.setLaserFrameId(parameters_client->get_parameter<std::string>("laser_frame_id","laser"));
 
   // Run the urg node
   urgNode.run();
